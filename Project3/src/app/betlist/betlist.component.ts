eb2fb46097fb3594a71b3220dcea8b35f09e7ebe
suint <lui.ysia@gmail.com>
2020-04-29 15:50:26 -0400

    active tabs

--- Project3/src/app/betlist/betlist.component.ts
+++ Project3/src/app/betlist/betlist.component.ts
@@ -20,18 +20,24 @@ export class BetlistComponent implements OnInit {
     {"bets": 1643, "name": "Promotion to assistant manager", "user":"futuremanager", "cutoff": this.dateFormat("1988-02-26T11:30+01:00")}
   ]
   pipe = new DatePipe('en-US');
+  activeTab = "top-nav";
 
   constructor() {
     
   }
 
   dateFormat(myDate: string): string{
-    console.log(20312423);
     const formattedDate = formatDate(myDate, "medium", "en-US");
     return formattedDate;
   }
 
   ngOnInit(): void {
+  }
+
+  tabNav(tab: string): void { 
+    document.getElementById(this.activeTab).classList.remove("active");
+    this.activeTab = tab.concat('-nav');
+    document.getElementById(tab.concat('-nav')).classList.add('active'); //http://plnkr.co/edit/glSz1qytmdZ9BQfGbmVo?p=preview
   }
 
 }
