9665e90e1f0f24b904a8835f93cc5d9e1f645d3d
suint <lui.ysia@gmail.com>
2020-04-29 14:42:50 -0400

    added tabs

--- Project3/src/app/betlist/betlist.component.ts
+++ Project3/src/app/betlist/betlist.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { formatDate, DatePipe } from '@angular/common';
 
-type Bet = {
+type Bet = { //probably move this into another file at some point
   bets: number;
   name: string;
   user: string;
@@ -15,7 +15,7 @@ type Bet = {
 
 
 export class BetlistComponent implements OnInit {
-  betlist: Bet[] = [ //placeholder data delete later and replace w api call
+  betlist: Bet[] = [ //placeholder data delete later and replace w api callw
     {"bets": 2032, "name": "Baby Abigail's Eye Color", "user":"newdad110", "cutoff": this.dateFormat("1987-07-16T19:20+01:00")},
     {"bets": 1643, "name": "Promotion to assistant manager", "user":"futuremanager", "cutoff": this.dateFormat("1988-02-26T11:30+01:00")}
   ]
