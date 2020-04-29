b3e7e1e9b3025d7cc8214fae6f9b47a3bb2622d2
suint <lui.ysia@gmail.com>
2020-04-29 14:33:50 -0400

    betlist displays bets in array

--- Project3/src/app/betlist/betlist.component.ts
+++ Project3/src/app/betlist/betlist.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate, DatePipe } from '@angular/common';
 
+type Bet = {
+  bets: number;
+  name: string;
+  user: string;
+  cutoff: string;
+}
 @Component({
   selector: 'app-betlist',
   templateUrl: './betlist.component.html',
   styleUrls: ['./betlist.component.scss']
 })
-export class BetlistComponent implements OnInit {
 
-  constructor() { }
+
+export class BetlistComponent implements OnInit {
+  betlist: Bet[] = [ //placeholder data delete later and replace w api call
+    {"bets": 2032, "name": "Baby Abigail's Eye Color", "user":"newdad110", "cutoff": this.dateFormat("1987-07-16T19:20+01:00")},
+    {"bets": 1643, "name": "Promotion to assistant manager", "user":"futuremanager", "cutoff": this.dateFormat("1988-02-26T11:30+01:00")}
+  ]
+  pipe = new DatePipe('en-US');
+
+  constructor() {
+    
+  }
+
+  dateFormat(myDate: string): string{
+    console.log(20312423);
+    const formattedDate = formatDate(myDate, "medium", "en-US");
+    return formattedDate;
+  }
 
   ngOnInit(): void {
   }
