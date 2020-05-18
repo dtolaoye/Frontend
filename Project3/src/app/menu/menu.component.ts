63db51537ad21aed75754d43a17bae28db91d77f
BuildTools <ayasser@udel.edu>
2020-05-18 13:14:08 -0400

    more login stuff

--- Project3/src/app/menu/menu.component.ts
+++ Project3/src/app/menu/menu.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-
-
+import { Component, Input } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
- 
-  constructor() { }
-  @Input() activeClass = 'active';
-  ngOnInit(): void {
+
+export class MenuComponent {
+  title = 'Project3';
+
+  ngOnInit(): void{
+    
   }
 
-}
+  get loggedIn():boolean{
+    return this.authSvc.loggedIn;
+  }
+  constructor(public authSvc:AuthService) {
+    authSvc.authorize();
+  }
 
+  signout(){
+    this.authSvc.logout();
+    return false;
+  }
+  @Input() activeClass = 'active';
+}
